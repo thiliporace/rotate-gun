c8961cc5f4b878f5904d64593b0781a6cb63e228
thiliporace <85422792+thiliporace@users.noreply.github.com>
2023-05-09 20:54:49 +0000

    Initial commit

---
+++ RotateGun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGun : MonoBehaviour {
+
+    public Grappling grappling;
+
+    private Quaternion desiredRotation;
+    private float rotationSpeed = 5f;
+
+    void Update() {
+        if (!grappling.grappling)
+        {
+            desiredRotation = Quaternion.LookRotation(grappling.cam.forward);
+            transform.rotation = Quaternion.LookRotation(grappling.cam.forward);
+        }
+           
+        else
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, Time.deltaTime * rotationSpeed);
+            desiredRotation = Quaternion.LookRotation(grappling.grapplePoint - transform.position);
+            
+        }
+        
+    }
+
+}
